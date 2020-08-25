f02a77c473c521b8acac8c9e73c3a8e38635f70c
Link Li <link@langchao.org>
2020-08-24 23:26:23 -0400

    Updates

---
+++ precache-manifest.c2883f8d3dfe20d58b7ce6978f62c7ca.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa89d36dc823762632d14ef74848d50",
+    "url": "/sharkvenger/index.html"
+  },
+  {
+    "revision": "eba1e5cf8633d427db89",
+    "url": "/sharkvenger/static/css/2.d54bb455.chunk.css"
+  },
+  {
+    "revision": "eba1e5cf8633d427db89",
+    "url": "/sharkvenger/static/js/2.8b5e9b62.chunk.js"
+  },
+  {
+    "revision": "c346956c1449420dcbe7f2a51cdfc9c4",
+    "url": "/sharkvenger/static/js/2.8b5e9b62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e259e754120af0891ddf",
+    "url": "/sharkvenger/static/js/3.f9cea726.chunk.js"
+  },
+  {
+    "revision": "f4517de3281630c7ccb41daff707018a",
+    "url": "/sharkvenger/static/js/3.f9cea726.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a57ca30af4a4a3b1817a",
+    "url": "/sharkvenger/static/js/4.6192744f.chunk.js"
+  },
+  {
+    "revision": "2b5b7988c95576edbfbe",
+    "url": "/sharkvenger/static/js/5.4fb8029a.chunk.js"
+  },
+  {
+    "revision": "dffddbdbe739a544e077",
+    "url": "/sharkvenger/static/js/main.6f7b6c42.chunk.js"
+  },
+  {
+    "revision": "ccfb1c21edbb23f78888",
+    "url": "/sharkvenger/static/js/runtime-main.e1aabf1f.js"
+  }
+]);
