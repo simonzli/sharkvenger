cffc42b72fe1dce8df5631f41b75203c6638dddb
Link Li <link@langchao.org>
2020-08-31 01:33:16 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sharkvenger/precache-manifest.9949d89293cd15aafe52efd8617b2afc.js"
+  "/sharkvenger/precache-manifest.0a5b18d2d13a1637e933492769ccb8a8.js"
 );
 
 self.addEventListener('message', (event) => {
